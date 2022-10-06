6c8a4b0af7343f5d92d48796a1f5ca4bd5865127
yucarl77 <yucarl77@foxmail.com>
2022-10-06 22:49:24 +0800

    feat: register plugin event

--- src/callbacks/index.ts
+++ src/callbacks/index.ts
@@ -1,11 +1,13 @@
 import { StreamerItemTypes } from "../definitions/ItemTypes";
 
+samp.registerEvent("OnDynamicObjectMoved", "i");
 export const OnDynamicObjectMoved = (
   fn: (objectid: number) => number
 ): void => {
   samp.on("OnDynamicObjectMoved", fn);
 };
 
+samp.registerEvent("OnPlayerEditDynamicObject", "iiiiiiiii");
 export const OnPlayerEditDynamicObject = (
   fn: (
     playerid: number,
@@ -22,6 +24,7 @@ export const OnPlayerEditDynamicObject = (
   samp.on("OnPlayerEditDynamicObject", fn);
 };
 
+samp.registerEvent("OnPlayerSelectDynamicObject", "iiiiii");
 export const OnPlayerSelectDynamicObject = (
   fn: (
     playerid: number,
@@ -35,6 +38,7 @@ export const OnPlayerSelectDynamicObject = (
   samp.on("OnPlayerSelectDynamicObject", fn);
 };
 
+samp.registerEvent("OnPlayerShootDynamicObject", "iiiiii");
 export const OnPlayerShootDynamicObject = (
   fn: (
     playerid: number,
@@ -48,48 +52,56 @@ export const OnPlayerShootDynamicObject = (
   samp.on("OnPlayerShootDynamicObject", fn);
 };
 
+samp.registerEvent("OnPlayerPickUpDynamicPickup", "ii");
 export const OnPlayerPickUpDynamicPickup = (
   fn: (playerid: number, pickupid: number) => number
 ): void => {
   samp.on("OnPlayerPickUpDynamicPickup", fn);
 };
 
+samp.registerEvent("OnPlayerEnterDynamicCP", "ii");
 export const OnPlayerEnterDynamicCP = (
   fn: (playerid: number, checkpointid: number) => number
 ): void => {
   samp.on("OnPlayerEnterDynamicCP", fn);
 };
 
+samp.registerEvent("OnPlayerLeaveDynamicCP", "ii");
 export const OnPlayerLeaveDynamicCP = (
   fn: (playerid: number, checkpointid: number) => number
 ): void => {
   samp.on("OnPlayerLeaveDynamicCP", fn);
 };
 
+samp.registerEvent("OnPlayerEnterDynamicRaceCP", "ii");
 export const OnPlayerEnterDynamicRaceCP = (
   fn: (playerid: number, checkpointid: number) => number
 ): void => {
   samp.on("OnPlayerEnterDynamicRaceCP", fn);
 };
 
+samp.registerEvent("OnPlayerLeaveDynamicRaceCP", "ii");
 export const OnPlayerLeaveDynamicRaceCP = (
   fn: (playerid: number, checkpointid: number) => number
 ): void => {
   samp.on("OnPlayerLeaveDynamicRaceCP", fn);
 };
 
+samp.registerEvent("OnPlayerEnterDynamicArea", "ii");
 export const OnPlayerEnterDynamicArea = (
   fn: (playerid: number, areaid: number) => number
 ): void => {
   samp.on("OnPlayerEnterDynamicArea", fn);
 };
 
+samp.registerEvent("OnPlayerLeaveDynamicArea", "ii");
 export const OnPlayerLeaveDynamicArea = (
   fn: (playerid: number, areaid: number) => number
 ): void => {
   samp.on("OnPlayerLeaveDynamicArea", fn);
 };
 
+samp.registerEvent("OnPlayerGiveDamageDynamicActor", "iiiii");
 export const OnPlayerGiveDamageDynamicActor = (
   fn: (
     playerid: number,
@@ -102,30 +114,35 @@ export const OnPlayerGiveDamageDynamicActor = (
   samp.on("OnPlayerGiveDamageDynamicActor", fn);
 };
 
+samp.registerEvent("OnDynamicActorStreamIn", "ii");
 export const OnDynamicActorStreamIn = (
   fn: (actorid: number, forplayerid: number) => number
 ): void => {
   samp.on("OnDynamicActorStreamIn", fn);
 };
 
+samp.registerEvent("OnDynamicActorStreamOut", "ii");
 export const OnDynamicActorStreamOut = (
   fn: (actorid: number, forplayerid: number) => number
 ): void => {
   samp.on("OnDynamicActorStreamOut", fn);
 };
 
+samp.registerEvent("Streamer_OnItemStreamIn", "iii");
 export const Streamer_OnItemStreamIn = (
   fn: (type: StreamerItemTypes, id: number, forplayerid: number) => number
 ): void => {
   samp.on("Streamer_OnItemStreamIn", fn);
 };
 
+samp.registerEvent("Streamer_OnItemStreamOut", "iii");
 export const Streamer_OnItemStreamOut = (
   fn: (type: StreamerItemTypes, id: number, forplayerid: number) => number
 ): void => {
   samp.on("Streamer_OnItemStreamOut", fn);
 };
 
+samp.registerEvent("Streamer_OnPluginError", "s");
 export const Streamer_OnPluginError = (fn: (error: string) => number): void => {
   samp.on("Streamer_OnPluginError", fn);
 };
