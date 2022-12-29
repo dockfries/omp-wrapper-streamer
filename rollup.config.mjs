940fe1fe54d6d4e29df392842d5ec3a096d6e1f0
yucarl77 <yucarl77@foxmail.com>
2022-12-29 16:55:14 +0800

    chore: update dependency

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,10 +1,11 @@
 import esbuild from "rollup-plugin-esbuild";
-import { typescriptPaths } from "rollup-plugin-typescript-paths";
 import del from "rollup-plugin-delete";
 import externals from "rollup-plugin-node-externals";
 import dts from "rollup-plugin-dts";
 
-import { compilerOptions } from "./tsconfig.json";
+import { createRequire } from "node:module";
+const require = createRequire(import.meta.url);
+const { compilerOptions } = require("./tsconfig.json");
 
 const inputPath = "./src/main.ts";
 const outputPath = "./dist";
@@ -19,7 +20,6 @@ export default [
     plugins: [
       del({ targets: outputPath + "/*" }),
       esbuild({ minify: true }),
-      typescriptPaths({ preserveExtensions: true }),
       externals(),
     ],
   },
